37221cf3b86b1b26f5eca44c1f11a916f2bdd675
aneekamemon <aneekamemon13@gmail.com>
2024-10-16 14:43:31 +0400

    Create Coding is cool.py
    Coding is cool - Aneeka Memon

---
+++ Exercise 01/Coding is cool.py
@@ -0,0 +1,16 @@
+# Coding is cool (assignment)
+
+# Assigning the word 'coding' to the variable 'word1'
+word1 = "coding"
+
+# Assigning the word 'is' to the variable 'word2'
+word2 = "is"
+
+# Assigning the word 'cool' to the variable 'word3'
+word3 = "cool"
+
+# Combine the variables through string concentration with spaces between them.
+
+print (word1 + " " + word2 + " " + word3) 
+
+ # Which will print the result 'coding is cool' in the terminal.
