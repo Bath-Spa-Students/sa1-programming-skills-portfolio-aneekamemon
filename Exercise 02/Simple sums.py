566f9fe2db855bc47bff7714524be0c7ce42f369
aneekamemon <aneekamemon13@gmail.com>
2024-10-24 01:41:13 +0400

    Create Simple sums.py

---
+++ Exercise 02/Simple sums.py
@@ -0,0 +1,17 @@
+# Exercise 2: Simple Sums 
+
+# Declare a variable and initialize it with the integer value '8'.
+
+first_number = (8)
+
+# Declare a second variable and initialize it with the integer value '10'.
+
+second_number = (10)
+
+# Declare a third variable that stores the sum of the initial 2 numbers. 
+
+sum_of_the_numbers = (first_number + second_number)
+
+# Print the value of the sum to the console. 
+
+print (int(sum_of_the_numbers), ("Is the sum of the first and second number."))
