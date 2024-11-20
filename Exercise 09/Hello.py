81f22fbe5a30775dae10302b0a4aa2ac25427771
aneekamemon <aneekamemon13@gmail.com>
2024-11-20 04:15:20 +0400

    Hello.py

---
+++ Exercise 09/Hello.py
@@ -0,0 +1,26 @@
+# Hello 
+
+
+'''
+
+Fill in the blanks in the code below so that the function hello() prints “Hello”
+to the console.
+
+def hello():
+____ # Fill in this blank to print "Hello" to the console
+def main():
+____ # Fill in this blank to call the hello() function
+if __name__ == "__main__":
+main()
+# This has been copied from the exercise sheet. 
+
+'''
+
+def hello():
+    print ("Hello") # This will print out the word "Hello"
+
+def main():
+    hello()     # hello() with the brackets calls the function to be executed
+
+if __name__ == "__main__":
+    main()      # This calls the 'main()' function...
