2895b5c17498f1180f2f43d54ed5c3c648a05d21
aneekamemon <aneekamemon13@gmail.com>
2024-11-21 14:25:09 +0400

    Turtles.py

---
+++ CLASS PRACTICE/Turtles.py
@@ -0,0 +1,12 @@
+
+# TURTLES CLASS PRACTICE THU 14TH NOV 2024
+
+import turtle 
+turtle.forward(100)
+turtle.left(80)
+turtle.forward(100)
+
+import turtle
+turtle.pensize(5)
+turtle.pencolor('blue')
+turtle.circle(100)
