c38919017f79eb176c4b2a49e07414c04407392a
aneekamemon <aneekamemon13@gmail.com>
2024-11-21 14:17:09 +0400

    Calculations.py

---
+++ CLASS PRACTICE/Calculations.py
@@ -0,0 +1,33 @@
+
+#Performing calculations through operators 
+
+print (5+6)
+print (15-11)
+print (7*6)
+print (50/2)  # for floating point 
+print (34//2) # for integer 
+
+
+# Operator procedure with BODMAS (Bracket, order, division, multiplication, addition, subtraction).
+
+print ((40/5)*8 -(27+4))
+
+#Use back slash \ to write the formula in multiple lines
+
+result = 7 * 4 + 5 * 2 + \
+        5 * 8 + 3 * 4
+print (result)
+
+# End delimiter to remove new line, so by default print will switch to another statement. 
+
+First_name = "Aneeka Memon"
+print (First_name , end = " " )
+print ("new line message")
+
+#Performing calculations through operators 
+
+print (5+6)   # the operator is addition 
+print (15-11) # the operator is subtraction 
+print (7*6)   # the operator is multiplication
+print (49/2)  # for floating point (division)
+print (34//2) # for integer (diviison)
