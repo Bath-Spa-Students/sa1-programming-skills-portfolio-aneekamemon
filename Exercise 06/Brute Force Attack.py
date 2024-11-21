e49150726f9e17812886dbc0bb7d3b32e787dd0e
aneekamemon <aneekamemon13@gmail.com>
2024-11-21 22:47:00 +0400

    Brute Force Attack.py

--- Exercise 06/Brute Force Attack.py
+++ Exercise 06/Brute Force Attack.py
@@ -2,9 +2,6 @@
 '''
 Write a program that simulates a password entry system. The correct password
 is defined as 12345. The program should keep asking the user to enter the
-
-3
-
 password until they provide the correct one.
 Basic Requirements:
 1. Define the correct password.
@@ -25,8 +22,7 @@ maximum_num_of_attempts = 10                # This code sets the maximum passwor
 
 while num_of_attempts < maximum_num_of_attempts:      # This creates a loop that continues until a correct password is entered, as it asks the user for the correct password...
 
-    user_password = input("Enter the correct password: ").upper().strip()      # upper.() allows for the text to be in all caps 
-
+    user_password = input("Enter the correct password: ")
     num_of_attempts += 1        # This adds an attempt count onto the counter, after an attempt has been made
 
     if user_password == correct_password:       # This alerts the user that the correct password has been entered, and will break the loop.
