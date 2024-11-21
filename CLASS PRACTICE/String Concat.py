b2c4aa540a27026189a152adf9f6441819d4f9f1
aneekamemon <aneekamemon13@gmail.com>
2024-11-21 14:23:07 +0400

    String Concat.py

---
+++ CLASS PRACTICE/String Concat.py
@@ -0,0 +1,25 @@
+#String Concentration through variable
+message = "how " + 'are you?'
+print (message)
+
+
+#String Concat through print statement 
+print ('Todays date is; Thursday ' +
+        'the third of October ' +
+        '2024.')   
+
+
+#String concat through different variables
+a = "Pakistan"
+b = "zindabad"
+c = a + " " + b
+print(c)
+
+
+
+#string concat through different additional variables 
+a = "today"
+b = "was"
+c = "great"
+d = a + " " + b + " " + c
+print(d)
