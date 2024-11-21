22ba5d11bb28eedf9a436cfccc6a639f8bdba6eb
aneekamemon <aneekamemon13@gmail.com>
2024-11-22 03:55:58 +0400

    Days of the month.py

--- Exercise 05/Days of the month.py
+++ Exercise 05/Days of the month.py
@@ -24,18 +24,18 @@ and print the number of days in the corresponding month.
 days_of_the_month = {
 
 # num of days in the months  # MONTHS
-1: 31,                  # January
-2: 28,                  # February
-3: 31,                  # March
-4: 30,                  # April
-5: 31,                  # May
-6: 30,                  # June
-7: 31,                  # July
-8: 31,                  # August
-9: 30,                  # September
-10: 31,                 # October
-11: 30,                 # November
-12: 31,                 # December
+1: 31,# January
+2: 28,# February
+3: 31,# March
+4: 30,# April
+5: 31,# May
+6: 30,# June
+7: 31,# July
+8: 31,# August
+9: 30,# September
+10: 31,# October
+11: 30,# November
+12: 31,# December
 
 }
 
