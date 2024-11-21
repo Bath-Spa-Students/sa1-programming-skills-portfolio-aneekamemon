a3bdf0d46c516ebe31201107090eafd115ca1280
aneekamemon <aneekamemon13@gmail.com>
2024-11-21 14:20:27 +0400

    Int and float.py

---
+++ CLASS PRACTICE/Int and float.py
@@ -0,0 +1,14 @@
+#To get input from the user (name)
+first_name = input ("Enter you name: ")
+print (first_name)
+
+Last_name = input ("Enter your last name: ")
+print (Last_name)
+
+#To get input from user: age. 
+Age = int(input ("How old are you? "))
+print (Age) 
+
+#To get input from user: Height. 
+height = float(input ("What is your height? "))
+print (height)
