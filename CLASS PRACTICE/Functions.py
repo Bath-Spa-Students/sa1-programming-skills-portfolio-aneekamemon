4db8d8489b95541a1d99be52541b918100db0eb6
aneekamemon <aneekamemon13@gmail.com>
2024-11-21 14:19:38 +0400

    Functions.py

---
+++ CLASS PRACTICE/Functions.py
@@ -0,0 +1,27 @@
+# Thursday 14th November 2024
+
+
+# What are functions? 
+# Functions are a group of statements within a program that perform as specific task,
+# Functions are used to divide and conquer a large task. 
+# To call a function is to target specific local variables 
+
+# A void function:
+# Simply executes the statements it contains and then terminates. 
+
+# A value-returning function:
+# Executes the statements it contains, and then it returns a value a value, 
+# back to the statement that called it. 
+
+
+
+def print_username():
+    username="aneeka"
+    print (username)
+
+def print_username_2():
+    username_2="maryam"
+    print (username_2)
+
+print_username()
+print_username_2()
