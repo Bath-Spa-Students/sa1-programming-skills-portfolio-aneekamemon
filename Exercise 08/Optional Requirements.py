00d54d1849ca87a364b7425e96229ac1472981d1
aneekamemon <aneekamemon13@gmail.com>
2024-11-23 02:24:32 +0400

    Optional Requirements.py

--- Exercise 08/Optional Requirements.py
+++ Exercise 08/Optional Requirements.py
@@ -12,12 +12,21 @@ Optional Requirements:
 names = ["Jake", "Zac", "Ian", "Ron", "Sam", "Dave"]
 
 specific_name_search = input("Enter the name you'd like to search for: ").strip().lower().upper() # This allows the user to type the name they are looking for, irrespective capitalization or lowercase lettering. 
+
 found = False
 
 for name in names:
-    if name.lower() == specific_name_search.lower():
-        found = True
-        print (f"The name {specific_name_search.lower()} is on the list!") # This is what will be printed out.
+
+
+
+    if name.lower() == (specific_name_search.strip().lower()):
+        
+        
+        print (f"The name {specific_name_search.strip().lower()} is on the list!") # This is what will be printed out.
+        
         break 
-if not found:
-    print (f"The name '{specific_name_search.lower()}' is NOT on the list... ") # If the name is not found, this will print out.
+
+    
+else:
+    
+    print (f"The name '{specific_name_search.strip().lower()}' is NOT on the list... ") # If the name is not found, this will print out.
