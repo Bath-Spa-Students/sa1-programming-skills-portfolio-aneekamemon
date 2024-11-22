f244cb443faf3a3dbe303b52a2d0fdca7729f07c
aneekamemon <aneekamemon13@gmail.com>
2024-11-23 03:07:49 +0400

    Strings Concatenation.py

--- Z.Bonus.Exercises/Strings Concatenation.py
+++ Z.Bonus.Exercises/Strings Concatenation.py
@@ -1,1 +1,19 @@
+# Strings Concatenation
 
+
+
+'''
+Write three strings in different variables and print the output as one string.
+'''
+
+# Define the 3 strings in 3 different variables
+
+word1 = "Hi,"   # Define word 1 with a term
+
+word2 = "How are"   # Define word 2 with a term
+
+word3 = "You?"   # Define word 3 with a term
+
+Sentence = word1 + " " + word2 + " " + word3    # To concatenate the strings add a plus sign
+
+print (Sentence)    # Use print to place the output in the terminal
