b9973c36ee5aebc5bac5b215202522d06785cce7
aneekamemon <aneekamemon13@gmail.com>
2024-11-23 19:05:12 +0400

    Functions_favorite_book.py

--- Z.Bonus.Exercises/Functions_favorite_book.py
+++ Z.Bonus.Exercises/Functions_favorite_book.py
@@ -13,3 +13,23 @@ Call the function, making sure to include a book title as an argument in the fun
 
 '''
 
+
+booktitle = "VarJak Paw"        # Title of the book..
+
+
+def print_favorite_book(booktitle):          # This is the parameter which is the title
+
+    print (f"My brother's favorite book is, {booktitle}!")  # This is what should be printed out...
+
+def main():     # This is the main function which is defined 
+
+
+    booktitle = "VarJak Paw"        # Title of the book..
+
+
+    print_favorite_book(booktitle)  
+
+
+
+if __name__ == "__main__":
+    main()      # If run directly, this will run in the main function.
