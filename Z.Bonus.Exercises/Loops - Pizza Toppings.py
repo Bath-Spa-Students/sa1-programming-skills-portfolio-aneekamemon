b10dfffc176590c6e5fa46a127f9ca13d422d0de
aneekamemon <aneekamemon13@gmail.com>
2024-11-23 18:08:16 +0400

    Loops - Pizza Toppings.py

--- Z.Bonus.Exercises/Loops - Pizza Toppings.py
+++ Z.Bonus.Exercises/Loops - Pizza Toppings.py
@@ -1,1 +1,17 @@
+# Loops - Pizza Toppings:
 
+'''
+
+You should have keys such as first_name, last_name, age, and city. Print each piece of
+information stored in your dictionary.
+
+Loops- Pizza Toppings :
+
+Write a loop that prompts the user to enter a series of pizza toppings until they enter a
+'quit' value. As they enter each topping.
+
+Print a message saying you’ll add that topping to their pizza.
+
+# This has been copied from the bonus exercise sheet.
+
+'''
