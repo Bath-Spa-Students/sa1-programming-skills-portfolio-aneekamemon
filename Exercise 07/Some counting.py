75d03d845a55b0860a8bf3dbdbc7937e81f44aea
aneekamemon <aneekamemon13@gmail.com>
2024-11-23 20:05:40 +0400

    Some counting.py

--- Exercise 07/Some counting.py
+++ Exercise 07/Some counting.py
@@ -15,25 +15,25 @@ loops in a single project
 
 print ("Counting up from 0 - 50: \n" )     # This loop counts upwards from 0 to 50 with the increments of 1. The \n allows for a gap. 
 
-for i in range (0, 51, 1):
-    print (i)
+for thenumbers in range (0, 51, 1):
+    print (thenumbers)
 
 print ("\n Counting down from 50 - 0: \n")     # This loop counts downwards from 50 to 0 with the increments of 1
 #The \n allows for a gap. 
-for i in range (50, -1, -1):
-    print (i)
+for thenumbers in range (50, -1, -1):
+    print (thenumbers)
 
 print ("\n Counting up from 30 - 50: \n")     # This loop counts upwards from 30 to 50 with the increments of 1
 #The \n allows for a gap. 
-for i in range (30, 51, 1):
-    print (i)
+for thenumbers in range (30, 51, 1):
+    print (thenumbers)
 
 print ("\n Counting down from 50 - 10: \n")     # This loop counts downwards from 50 to 10 with the increments of 2
 #The \n allows for a gap. 
-for i in range (50, 9, -2):
-    print (i)
+for thenumbers in range (50, 9, -2):
+    print (thenumbers)
 
 print ("\n Counting up from 100 - 200: \n")     # This loop counts upwards from 100 to 200 with the increments of 5
 #The \n allows for a gap. 
-for i in range (100, 201, 5):
-    print (i)
+for thenumbers in range (100, 201, 5):
+    print (thenumbers)
