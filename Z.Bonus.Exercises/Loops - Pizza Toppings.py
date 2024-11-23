2fdd28781d24301286a787a2edad37d12393aa01
aneekamemon <aneekamemon13@gmail.com>
2024-11-23 18:37:06 +0400

    Loops - Pizza Toppings.py

--- Z.Bonus.Exercises/Loops - Pizza Toppings.py
+++ Z.Bonus.Exercises/Loops - Pizza Toppings.py
@@ -12,3 +12,6 @@ Print a message saying you’ll add that topping to their pizza.
 # This has been copied from the bonus exercise sheet.
 
 '''
+
+
+pizza_toppings_list = []    # This is where user input for pizza toppings will be stored.
