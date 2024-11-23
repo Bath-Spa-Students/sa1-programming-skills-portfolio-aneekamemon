ceda43640f78fcd67109552b88b950465194ff50
aneekamemon <aneekamemon13@gmail.com>
2024-11-23 15:44:13 +0400

    Variables_fav_number.py

--- Z.Bonus.Exercises/Variables_fav_number.py
+++ Z.Bonus.Exercises/Variables_fav_number.py
@@ -8,16 +8,17 @@ message that reveals your favorite number. Print that message.
 
 '''
 
-favorite_number = ("13")
+favorite_number = ("13")    # Define the favorite number as '13'.
 
 
 
+# Use a f-string which will allow the expressions of variables that are in {} curly brackets.
 
 my_message = (f"My favorite number would be the number {favorite_number}!")
 
+# The variable 'my message' is assigned with a sentence that includes the intital variable. 
 
 
 
-print (my_message)
-
+print (my_message)  # This will print out the message
 
