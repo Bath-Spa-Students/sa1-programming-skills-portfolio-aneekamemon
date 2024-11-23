f4766a6c75f6506845c9508768c455db688700e9
aneekamemon <aneekamemon13@gmail.com>
2024-11-23 17:27:27 +0400

    Control Structures.py

--- Z.Bonus.Exercises/Control Structures.py
+++ Z.Bonus.Exercises/Control Structures.py
@@ -20,3 +20,32 @@ user_input = input("\nEnter the color of the alien: ").lower() # .lower() allows
 
 points_gained = 0       # The starting amounts of points.
 
+
+if user_input == alien_colors[0]:   # for Green colored alien
+
+    points_gained =+ 5      # 5 points gained if an green alien is killed
+
+
+    print (f"\nYOU HAVE GAINED {points_gained} POINTS")
+
+
+elif user_input == alien_colors[1]: # for yellow colored alien
+
+    points_gained =+ 3      # 3 points gained if an yellow alien is killed
+
+
+    print (f"\nYOU HAVE GAINED {points_gained} POINTS")
+
+
+elif user_input == alien_colors[2]:     ## for red colored alien
+
+    points_gained =+ 1      # 1 point gained if an red alien is killed
+
+
+    print (f"\nYOU HAVE GAINED {points_gained} POINT")
+
+
+else:
+    print ("You haven't gained any points so far.")     # If a color is that isnt in the dictionary is typed in, then no points are to be given...
+
+
