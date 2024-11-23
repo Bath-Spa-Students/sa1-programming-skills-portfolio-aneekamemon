44b1563e1f45dd58a08e0080d713f837ace074e4
aneekamemon <aneekamemon13@gmail.com>
2024-11-23 15:46:44 +0400

    List names.py

--- Z.Bonus.Exercises/List names.py
+++ Z.Bonus.Exercises/List names.py
@@ -1,1 +1,9 @@
+# List - Names 
 
+'''
+
+Store the names of a few of your friends in a list called names. Print each person’s name
+by accessing each element in the list, one at a time.
+# This has been copied from the bonus exercise sheet.
+
+'''
