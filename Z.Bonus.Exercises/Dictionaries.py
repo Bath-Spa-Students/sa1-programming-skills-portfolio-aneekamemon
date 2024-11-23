f18d6e9bab5b9f6442f07bc7adfd8edf4bb11002
aneekamemon <aneekamemon13@gmail.com>
2024-11-23 18:04:07 +0400

    Dictionaries.py

--- Z.Bonus.Exercises/Dictionaries.py
+++ Z.Bonus.Exercises/Dictionaries.py
@@ -20,3 +20,16 @@ age = 15             # Age is a integer in this case
 
 # City they live in...
 city_name = "Karachi"
+
+# Use discriptive keys to store information in a dictionary 
+
+# use 'keys' correlated to their specific values
+
+information_dictionary = { 'fname': first_name, 'lname': last_name, 'age': age, 'name of their city': city_name}    # Use a dictionary with keys and items to store their information
+
+# Print values in seperate lines (through the use of '\n')
+
+# As well as using variables with appropriate data types. 
+
+print (f"\n{information_dictionary['fname']} \n\n{information_dictionary['lname']} \n\n{information_dictionary['age']} \n\n{information_dictionary['name of their city']}\n")
+# This print code will print out all the information about your person, in seperate lines.
