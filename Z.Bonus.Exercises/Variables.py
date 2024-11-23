202095597450b45fdcb697abefb8c6f165b6592c
aneekamemon <aneekamemon13@gmail.com>
2024-11-23 15:25:05 +0400

    Variables.py

---
+++ Z.Bonus.Exercises/Variables.py
@@ -0,0 +1,9 @@
+# Variables: 
+
+
+'''
+Assign a message to a variable, and print that message. Then change the value of the
+variable to a new message, and print the new message.
+# This has been copied from the bonus exercise sheet.
+
+'''
