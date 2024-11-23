8d13b8520915d13bf68668d85ecc79631883305a
aneekamemon <aneekamemon13@gmail.com>
2024-11-23 16:40:05 +0400

    List names.py

--- Z.Bonus.Exercises/List names.py
+++ Z.Bonus.Exercises/List names.py
@@ -11,3 +11,35 @@ by accessing each element in the list, one at a time.
 
 names = ["Maryam", "Farah", "Asna", "shaggu", "rahma"]  # This is the list that stores 5 different string variables.
 
+
+print("\n")             # This leaves a gap to make the names more readable.
+
+
+print (names [0])       # Specific names are called out one at a time through indexing, (names [0]) will find the first name on the list.
+
+print("------")         # This creates a line underneath the name to make it look neat. 
+print("\n")             # This leaves a gap to make the names more readable.
+
+
+print (names [1])       # Specific names are called out one at a time through indexing, (names [1]) will find the second name on the list.
+
+print("-----")         # This creates a line underneath the name to make it look neat. 
+print("\n")             # This leaves a gap to make the names more readable.
+
+
+print (names [2])       # Specific names are called out one at a time through indexing, (names [2]) will find the third name on the list.
+
+print("-----")         # This creates a line underneath the name to make it look neat. 
+print("\n")             # This leaves a gap to make the names more readable.
+
+
+print (names [3])       # Specific names are called out one at a time through indexing, (names [3]) will find the fourth name on the list.
+
+print("-------")         # This creates a line underneath the name to make it look neat. 
+print("\n")             # This leaves a gap to make the names more readable.
+
+
+print (names [4])       # Specific names are called out one at a time through indexing, (names [4]) will find the fifth name on the list.
+
+print("-----")         # This creates a line underneath the name to make it look neat. 
+print("\n")             # This leaves a gap to make the names more readable.
