92f1f013baa60fdaa48a0aecb176222b5794025e
aneekamemon <aneekamemon13@gmail.com>
2024-11-23 17:48:53 +0400

    Dictionaries.py

--- Z.Bonus.Exercises/Dictionaries.py
+++ Z.Bonus.Exercises/Dictionaries.py
@@ -3,6 +3,7 @@
 '''
 Use a dictionary to store information about a person you know.Store their first name,
 last name, age, and the city in which they live.
+You should have keys such as first_name, age, and city. Print each piece of information stored in your dictionary.
 
-
+# This has been taken from the bonus exercise sheet.
 '''
